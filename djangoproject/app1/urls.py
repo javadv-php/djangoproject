9797fdb39714e724b5726b9eaf09007872709ff7
javadv-php <javadvayeri@gmail.com>
2021-08-25 14:35:23 +0530

    fourth commit

--- djangoproject/app1/urls.py
+++ djangoproject/app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('',views.TestFun,name="test")
+   path('',views.TestFun,name="test"),
+   path('html',views.Fn2,name="html")
 ]
--- djangoproject/app1/views.py
+++ djangoproject/app1/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def TestFun(request):
     return HttpResponse('function is working')
+def Fn2(request):
+    return render(request,'login.html') 
     
