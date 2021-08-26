81d38cb4ff9cb6bafb531fa0b99a1baea120ddc3
javadv-php <javadvayeri@gmail.com>
2021-08-26 17:32:57 +0530

    7th commit

--- djangoproject/app1/urls.py
+++ djangoproject/app1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('',views.TestFun,name="test"),
-   path('html',views.Fn2,name="html")
+   path('html',views.Fn2,name="html"),
+   path('regform',views.RegFun,name="regform"),
 ]
--- djangoproject/app1/views.py
+++ djangoproject/app1/views.py
@@ -6,4 +6,5 @@ def TestFun(request):
     return HttpResponse('function is working')
 def Fn2(request):
     return render(request,'login.html') 
-    
+def RegFun(request):
+    return render(request,'registration.html')
